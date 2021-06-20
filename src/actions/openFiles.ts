1eac0386edca596367629274642c862ffb13c36c
jopemachine <jopemachine@naver.com>
2021-06-20 21:48:23 +0900

    Implement url encoding logic to open action

--- src/actions/openFiles.ts
+++ src/actions/openFiles.ts
@@ -1,9 +1,14 @@
+import encodeUrl from 'encodeurl';
+import isUrl from 'is-url';
 import open from 'open';
 
 /**
  * @param  {string} filePath
  */
 const openFile = async (filePath: string): Promise<void> => {
+  if (isUrl(filePath)) {
+    filePath = encodeUrl(filePath);
+  }
   await open(filePath, { wait: false });
 };
 
