cc863ef76cd31fce20d44d3c698c729effdf38b3
jopemachine <jopemachine@naver.com>
2021-06-23 02:03:52 +0900

    Update encoding method

--- src/actions/openFiles.ts
+++ src/actions/openFiles.ts
@@ -6,6 +6,8 @@ import open from 'open';
  * @param  {string} filePath
  */
 const openFile = async (filePath: string): Promise<void> => {
+  // Replace whitespace with encoded whitespace
+  filePath = filePath.split(' ').join('%20');
   if (isUrl(filePath)) {
     filePath = encodeUrl(filePath);
   }
