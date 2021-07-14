a967271f1d853b4fc5d6a7f6a6e43bdfad6949dd
jopemachine <jopemachine@naver.com>
2021-07-14 18:09:19 +0900

    hotfix

--- src/actions/openFiles.ts
+++ src/actions/openFiles.ts
@@ -8,8 +8,9 @@ import open from 'open';
 export const openFile = async (filePath: string): Promise<void> => {
   // Replace whitespace with encoded whitespace if there is.
   // If not want this encoding method, replace it in extension before passing it in this.
-  if (isUrl(filePath)) {
-    filePath = filePath.split(' ').join('%20');
+  const assumeUrl = filePath.split(' ')[0];
+
+  if (isUrl(assumeUrl)) {
     filePath = encodeUrl(filePath);
   }
 
